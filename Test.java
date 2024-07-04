ffe30da8642729614bc52c1e4cb03b201781d422
Reena-Puram <reenapuram5b1@gmail.com>
2024-07-04 15:34:50 +0530

    Instance variables

---
+++ Test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.String;
+class Sample	
+{
+int a=10;
+}
+class Test
+{
+public static void main(String args[])
+{
+Sample s1=new Sample();
+System.out.println(s1.a);
+Sample s2=new Sample();
+System.out.println(s2.a);
+}
+}
