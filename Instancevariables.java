c813c2adcbe1cb9410d2a4630182d70af560e1b2
Reena-Puram <reenapuram5b1@gmail.com>
2024-07-04 15:44:23 +0530

    default values

---
+++ Instancevariables.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.String;
+class Sample	
+{
+int a;
+double b;
+boolean c;
+String d;
+}
+class Instancevariables
+{
+public static void main(String args[])
+{
+Sample s1=new Sample();
+System.out.println(s1.a);
+System.out.println(s1.b);
+System.out.println(s1.c);
+System.out.println(s1.d);
+}
+}
